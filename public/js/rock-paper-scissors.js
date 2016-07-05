33b34c57fe4d69b87bc262146f6b9497af73d01b
Shota Onishi <showta.0804@gmail.com>
2016-07-05 20:49:38 +0900

    first commit

---
+++ public/js/rock-paper-scissors.js
@@ -0,0 +1,112 @@
+jQuery(function($){
+  "use strict";
+  var HAND_TYPE = [ "rock" , "scissors" , "paper" ];
+  var RESULT_CODE = { DRAW : 0, WIN : 1, LOSE : 2, };
+  var RESULT_MESSAGE = [ "draw.","You win!","You lose!" ];
+  var current_score = 1000;
+
+  var BOB_NEXT_HAND = HAND_TYPE[0];
+  var SHINGAN_ENALBE = false;
+  var SHINGAN_NUM = 0;
+
+  $(function() {
+      $.ajax({
+          url: '/api/missions',
+          type: 'GET',
+          dataType: 'json'
+      }).done(function(json, statusText, jqXHR) {
+          var data = $.parseJSON(json);
+          $(".mission1").append("<li>" + data.mission + "</li>");
+      }).fail(function(jqXHR, statusText, errorThrown) {
+          alert("データ取得失敗");
+      }).always(function() {
+      });
+  });
+  
+  $('#button').click(function(){
+      $('#main').toggle(function(){
+          if ($(this).is(':visible')) {
+              $('#button').text('非表示');
+          } else {
+              $('#button').text('表示');
+          }
+      });
+  });
+
+  $(".rsp-btn").click(function(){
+    var opponentHand = bobHand();
+
+    if(SHINGAN_ENALBE) {
+      opponentHand = BOB_NEXT_HAND;
+    }
+
+    var result = judge( $(this).attr("id"), opponentHand);
+    if (result != RESULT_CODE['LOSE']) {
+    }
+
+    $("#myrspimg").attr("src", "img/" + $(this).attr("id") + ".png");
+    $("#bobrspimg").attr("src", "img/" + opponentHand + ".png");
+    $("#result").text(RESULT_MESSAGE[result]);
+    $(".game_hands").toggle(function() {
+          
+    });
+
+    SHINGAN_ENALBE = false;
+  });
+
+  $("#shingan").click(function(){
+
+    if( SHINGAN_ENALBE ) {
+        return;
+    }
+
+    SHINGAN_ENALBE = true;
+    BOB_NEXT_HAND = bobHand();
+
+    // 5回で心眼発生確率0.5になる
+    if(Math.random() < Math.exp(Math.log(0.5)/5*SHINGAN_NUM) ){
+        $("#kokoro").attr("src", "img/" + BOB_NEXT_HAND + ".png");
+        SHINGAN_NUM++;
+    } else {
+        if(Math.random() < 0.5){
+            $("#kokoro").attr("src", "img/beer.png");
+        } else {
+            $("#kokoro").attr("src", "img/sushi.png");
+        }
+    }
+
+    $("#kokoro").animate({ 
+        left:"1095px"
+    }, 1000 ).animate({
+        left:"-103px"
+    }, 0);
+
+  });
+  
+  $(".score-btn").click(function() {
+      if ($(this).attr("id") == "down") {
+
+      }
+      $(".game_hands").toggle(function() {
+          
+      });
+  });
+
+  function bobHand() {
+    return HAND_TYPE[ Math.floor(Math.random() * 3) ];
+  }
+
+  function judge(myHand, opponentHand) {
+    var result;
+    if (myHand === opponentHand) {
+      result = RESULT_CODE.DRAW;
+    } else if ((myHand === HAND_TYPE[0] && opponentHand === HAND_TYPE[1]) ||
+               (myHand === HAND_TYPE[1] && opponentHand === HAND_TYPE[2]) || 
+               (myHand === HAND_TYPE[2] && opponentHand === HAND_TYPE[0])) {
+      result = RESULT_CODE.WIN;
+    }else {
+      result = RESULT_CODE.LOSE;
+    }
+    return result;
+  }
+});
